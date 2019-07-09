73809ff83e5f36d906cb4a90b87c22ef3b701626
Tyler Hawkins <thawkins@instructure.com>
2019-07-09 10:59:51 -0600

    fixed dev servers for 06

--- 06-webpack-dynamic-imports/webpack.config.dev.js
+++ 06-webpack-dynamic-imports/webpack.config.dev.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './'
+    publicPath: '/'
   },
   plugins: [
     new CleanWebpackPlugin(),
