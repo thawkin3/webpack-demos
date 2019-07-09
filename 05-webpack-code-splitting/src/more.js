1eefc6ad703651902a84143b862b2a9cc10ddd74
Tyler Hawkins <thawkins@instructure.com>
2019-07-09 11:02:18 -0600

    fixed dev servers for 05

--- 05-webpack-code-splitting/src/more.js
+++ 05-webpack-code-splitting/src/more.js
@@ -3,5 +3,5 @@ import _ from 'lodash';
 // this file uses lodash, and so do index.js and another.js
 
 console.log(
-  _.join(['Another', 'module', 'loaded!'], ' ')
+  _.join(['More', 'modules', 'loaded!'], ' ')
 );
--- 05-webpack-code-splitting/webpack.config.dev.js
+++ 05-webpack-code-splitting/webpack.config.dev.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './'
+    publicPath: '/'
   },
   plugins: [
     new CleanWebpackPlugin(),
